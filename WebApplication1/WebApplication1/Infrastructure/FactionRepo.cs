394f9e1c7000b2b05ebd7e7108a9e0001b8efe63
AlexsaniKY <cerafem@gmail.com>
2016-07-30 18:23:47 -0400

    created FactionRepo stubs

---
+++ WebApplication1/WebApplication1/Infrastructure/FactionRepo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Domain;
+
+namespace WebApplication1.Infrastructure
+{
+    public static class FactionRepo
+    {
+        static FactionRepo()
+        {
+        }
+
+        public static bool HasFaction(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool AddFaction(Faction newFaction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool UpdateFaction(Faction alteredFaction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool DeleteFaction(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Faction GetFaction(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
