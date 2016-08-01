5e7d4091b6d67800c04aa964b12212aacb7dee15
AlexsaniKY <cerafem@gmail.com>
2016-08-01 04:07:17 -0400

    Implemented Classes to begin using EF: AppDbContext and GenericRepository to inherit Repos from

---
+++ WebApplication1/WebApplication1/Infrastructure/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApplication1.Domain;
+using WebApplication1.Domain.Creature;
+using WebApplication1.Domain.Items;
+using WebApplication1.Domain.Locations;
+
+namespace WebApplication1.Infrastructure
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext() : base("AppDbContext") { }
+
+        public static AppDbContext Create()
+        {
+            return new AppDbContext();
+        }
+        //Creatures
+        DbSet<Monster> Monsters { get; set; }
+        DbSet<Player>Players { get; set; }
+        //Items
+        DbSet<Potion>Potions { get; set; }
+        DbSet<Weapon>Weapons { get; set; }
+        //Locations
+        DbSet <City> Cities { get; set; }
+        DbSet<Road> Roads { get; set; }
+
+        DbSet<Faction> Factions { get; set; }
+
+    }
+}
---
+++ WebApplication1/WebApplication1/Infrastructure/GenericRepository.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Linq;
+using WebApplication1.Domain.Interfaces;
+
+namespace WebApplication1.Infrastructure
+{
+    public class GenericRepository<T> where T : class, IDbEntity, IActivatable
+    {
+
+        protected DbContext _db;
+
+        protected IQueryable<T> Table
+        {
+            get
+            {
+                return _db.Set<T>();
+            }
+        }
+
+        public GenericRepository(DbContext db)
+        {
+            _db = db;
+        }
+
+        public IQueryable<T> List()
+        {
+            return from e in Table
+                   where e.Active
+                   select e;
+        }
+
+        public IQueryable<T> Get(int id)
+        {
+            return from e in Table
+                   where e.Active && e.Id == id
+                   select e;
+        }
+
+        public int Add(T entity)
+        {
+            _db.Set<T>().Add(entity);
+
+            return entity.Id;
+        }
+
+        public void Delete(int id)
+        {
+            Delete(Get(id).FirstOrDefault());
+        }
+        public void Delete(T entity)
+        {
+            entity.Active = false;
+        }
+
+        public void SaveChanges()
+        {
+            try
+            {
+                _db.SaveChanges();
+            }
+            catch (DbEntityValidationException dbError)
+            {
+                var firstError = dbError.EntityValidationErrors.First().ValidationErrors.First().ErrorMessage;
+                throw new ValidationException(firstError);
+            }
+        }
+    }
+}
