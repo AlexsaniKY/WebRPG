716796531b33e5a9eefca1ead30759ceb5c22983
AlexsaniKY <alexsaniwilliams@gmail.com>
2016-08-02 16:12:30 -0400

    offloaded service layer functionality into genericservices to avoid duplicated code

---
+++ WebApplication1/WebApplication1/Services/GenericServices.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Domain.Interfaces;
+using WebApplication1.Infrastructure;
+
+namespace WebApplication1.Services
+{
+    public class GenericServices<T> where T: class, IDbEntity, IActivatable
+    {
+        internal GenericRepository<T> _repo;
+
+        public T Get(int id)
+        {
+            var query = _repo.Get(id);
+            return (from t in query select t).FirstOrDefault();
+        }
+
+        public void Update(T alteredentity)
+        {
+            _repo.Update(alteredentity);
+            _repo.SaveChanges();
+        }
+
+        public void Add(T newentity)
+        {
+            _repo.Add(newentity);
+            _repo.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            _repo.Delete(id);
+            _repo.SaveChanges();
+        }
+
+        public IEnumerable<T> Enumerate() { return _repo.Enumerate(); }
+    }
+}
--- WebApplication1/WebApplication1/Services/PlayerServices.cs
+++ WebApplication1/WebApplication1/Services/PlayerServices.cs
@@ -8,62 +8,58 @@ using WebApplication1.Services.DTO;
 
 namespace WebApplication1.Services
 {
-    public class PlayerServices
+    public class PlayerServices: GenericServices<Player>
     {
-        internal PlayerRepo _repo;
-        internal WeaponServices weaponservices;
+        //internal WeaponServices weaponservices;
 
         public PlayerServices()
         {
             _repo = new PlayerRepo(AppDbContext.Create());
-            weaponservices = new WeaponServices();
+            //weaponservices = new WeaponServices();
         }
         
-        public  Player GetPlayerEntity(int id)
-        {
+        //public  Player GetPlayerEntity(int id) { 
+        //    return Get(id);
+        //}
 
-            var query = _repo.Get(id);
-            return (from p in query select p).FirstOrDefault();
-        }
+        //public Player GetPlayer(int id)
+        //{
+        //    var query = _repo.Get(id);
+        //    return new Player((from p in query
+        //            select new PlayerDTO()
+        //            {Id = p.Id,
+        //            Active = p.Active,
+        //            Name = p.Name,
+        //            FactionId = p.FactionId,
+        //            HP = p.HP,
+        //                WieldedWeaponId = p.WieldedWeaponId,
+        //                WieldedWeapon = new WeaponDTO()
+        //            {
+        //                Id = p.WieldedWeapon.Id,
+        //                Active = p.WieldedWeapon.Active,
+        //                Name = p.WieldedWeapon.Name,
+        //                Damage = p.WieldedWeapon.Damage
+        //            }
+        //            }).FirstOrDefault());
+        //}
 
-        public Player GetPlayer(int id)
-        {
-            var query = _repo.Get(id);
-            return new Player((from p in query
-                    select new PlayerDTO()
-                    {Id = p.Id,
-                    Active = p.Active,
-                    Name = p.Name,
-                    FactionId = p.FactionId,
-                    HP = p.HP,
-                        WieldedWeaponId = p.WieldedWeaponId,
-                        WieldedWeapon = new WeaponDTO()
-                    {
-                        Id = p.WieldedWeapon.Id,
-                        Active = p.WieldedWeapon.Active,
-                        Name = p.WieldedWeapon.Name,
-                        Damage = p.WieldedWeapon.Damage
-                    }
-                    }).FirstOrDefault());
-        }
+        //public void UpdatePlayer(Player updatedplayer) {
+        //    _repo.Update(updatedplayer);
+        //    _repo.SaveChanges();
+        //}
 
-        public void UpdatePlayer(Player updatedplayer) {
-            _repo.Update(updatedplayer);
-            _repo.SaveChanges();
-        }
+        //public void AddPlayer(Player newplayer)
+        //{
+        //    _repo.Add(newplayer);
+        //    _repo.SaveChanges();
+        //}
 
-        public void AddPlayer(Player newplayer)
-        {
-            _repo.Add(newplayer);
-            _repo.SaveChanges();
-        }
+        //public void DeletePlayer(int id)
+        //{
+        //    _repo.Delete(id);
+        //    _repo.SaveChanges();
+        //}
 
-        public void DeletePlayer(int id)
-        {
-            _repo.Delete(id);
-            _repo.SaveChanges();
-        }
-
-        public IEnumerable<Player> EnumerateAll() { return _repo.Enumerate(); }
+        //public IEnumerable<Player> EnumerateAll() { return _repo.Enumerate(); }
     }
 }
